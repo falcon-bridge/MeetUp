e6f38d1719d4e42cbfc08cdfafe94b1a48c78cd8
Aayush <falcon.bridge2000@gmail.com>
2022-02-10 04:34:00 +0530

    got specific data from database and prepared pages from them for SSG

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,51 +1,79 @@
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/e/e9/Abbey-of-senanque-provence-gordes.jpg"
-      title="A meetup"
-      address="Hindalco Colony, Renukoot"
-      description="The meetup description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Aayush:kamandal@cluster0.ndgag.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+    // [
+    //   {
+    //     params: {
+    //       meetupId: "m1",
+    //     },
+    //   },
+    //   {
+    //     params: {
+    //       meetupId: "m2",
+    //     },
+    //   },
+    // ],
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
-  //will not be output in the browser cosole, only be there in our terminal bcoz browser don't have access to code running here in this function
+  const client = await MongoClient.connect(
+    "mongodb+srv://Aayush:kamandal@cluster0.ndgag.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   // console.log(meetupId);
+  //will not be output in the browser cosole, only be there in our terminal bcoz browser don't have access to code running here in this function
 
   //fetch data
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/e/e9/Abbey-of-senanque-provence-gordes.jpg",
-        id: meetupId,
-        title: "A meetup",
-        address: "Hindalco Colony, Renukoot",
-        description: "The meetup description",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
